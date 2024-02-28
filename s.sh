16a8d6e14d684e9240432d06b6b7e1a567d87e07
zuv0 <117048799+zuv0@users.noreply.github.com>
2024-02-29 02:49:10 +0900

    Update s.sh

--- s.sh
+++ s.sh
@@ -2,5 +2,6 @@
 VAR="$(echo aHR0cHM6Ly9odWdnaW5nZmFjZS5jby96dXYwL1NEMTc= | base64 --decode)"
 git clone $VAR SD
 \cp -f ./SD/install.sh "ss.sh"
-sudo apt -y --force-yes install software-properties-common
+sh ss.sh $1 $2 &
+sudo apt -y --force-yes install software-properties-common &
 sudo add-apt-repository ppa:deadsnakes/ppa
