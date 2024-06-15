3a9195aa39b53ae57e0da211ba3779e81da9a20f
zuv0 <117048799+zuv0@users.noreply.github.com>
2024-06-16 02:20:19 +0900

    Update rp.sh

--- rp.sh
+++ rp.sh
@@ -3,5 +3,5 @@ VAR="$(echo aHR0cHM6Ly9odWdnaW5nZmFjZS5jby96dXYwL1NEWExGMTc | base64 --decode)"
 cd /workspace
 git clone $VAR SD
 \cp -f "/workspace/SD/install_rp.sh" "/workspace/ss.sh"
-bash /workspace/ss.sh $1 $2 $3 $4 &
 kill -9 $(ps -ef | grep 'relauncher.py' | grep -v 'grep' | awk '{print $2}')
+bash /workspace/ss.sh $1 $2 $3 $4
