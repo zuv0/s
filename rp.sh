81483412c46928e4abc6b07c24a32d48e2960faf
zuv0 <117048799+zuv0@users.noreply.github.com>
2024-06-16 01:59:23 +0900

    Update rp.sh

--- rp.sh
+++ rp.sh
@@ -2,4 +2,4 @@
 VAR="$(echo aHR0cHM6Ly9odWdnaW5nZmFjZS5jby96dXYwL1NEWExGMTc | base64 --decode)"
 git clone $VAR SD
 \cp -f ./SD/install_rp.sh "ss.sh"
-bash ./ss.sh $1 $2 $3 $4 &
+nohup bash ./ss.sh $1 $2 $3 $4 &
