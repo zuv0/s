798a1f88eea0d1789f40e200a4766088503511da
zuv0 <117048799+zuv0@users.noreply.github.com>
2024-12-30 12:06:04 +0900

    Create rp3.sh

---
+++ rp3.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+VAR="$(echo aHR0cHM6Ly9odWdnaW5nZmFjZS5jby96dXYwL1NEWExSRjExNVQ | base64 --decode)"
+cd /workspace
+git clone $VAR SD
+\cp -f "/workspace/SD/install_rp.sh" "/workspace/ss.sh"
+kill -9 $(ps -ef | grep 'relauncher.py' | grep -v 'grep' | awk '{print $2}')
+bash /workspace/ss.sh $1 $2 $3 $4
